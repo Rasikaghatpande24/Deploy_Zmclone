71c8131aedc4e425d6ad1717eebd97d5662d9fb0
Rasikaghatpande@24 <84622998+Rasikaghatpande24@users.noreply.github.com>
2021-11-17 14:25:16 +0530

    Update Wallpaper.jsx

--- src/Components/Wallpaper.jsx
+++ src/Components/Wallpaper.jsx
@@ -18,7 +18,7 @@ class Wallpaper extends React.Component {
         sessionStorage.setItem('locationId', locationId);
 
         axios({
-            url: `https://zmclone-backend.herokuapp.com/api/getRestaurantsbycity/${locationId}`,
+            url: `http://localhost:6503/api/getRestaurantsbycity/${locationId}`,
             method: 'GET',
             headers: { 'Content-Type': 'application/json' }
         })
