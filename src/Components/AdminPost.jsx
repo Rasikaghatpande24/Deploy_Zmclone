05f02984d7a39c3c62cc5e5cf6a9784a4d6acf42
Rasikaghatpande@24 <84622998+Rasikaghatpande24@users.noreply.github.com>
2021-11-17 14:07:52 +0530

    Update AdminPost.jsx

--- src/Components/AdminPost.jsx
+++ src/Components/AdminPost.jsx
@@ -14,7 +14,7 @@ class AdminPost extends React.Component{
     }
     componentDidMount() {
         axios({
-            url: 'http://localhost:6503/api/adminrest',
+            url: 'https://zmclone-backend.herokuapp.com/api/adminrest',
             method: 'GET',
             headers: { 'Content-Type': 'application/json' }
         })
